3e09bde17ef1433ad411f4696fe00fce0ded2beb
Alex McDowell <scruffyalexhs@gmail.com>
2020-11-12 10:20:59 -0700

    Add project files.

---
+++ Database/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Database.DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Database
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private GameTableAdapter gameTableAdapter = new GameTableAdapter();
+        private DataSet dataSet = new DataSet();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            gameTableAdapter.Fill(dataSet.Game);
+            DataContext = dataSet.Game.DefaultView;
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            gameTableAdapter.Update(dataSet);
+        }
+    }
+}
